d4b570360fa099fba9f5240affaf0d503a7e51cb
CBreakr <rob.notwicz@gmail.com>
2019-12-26 17:20:17 -0500

    delete comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+
+  ###
+  # action on new comment submit
   def create
     # remember to refer to the post id here
     @post = Post.find(params[:post_id])
@@ -7,6 +10,18 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  ###
+  # delete comment from the DB
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+  ###
+  # need to specify the fields
+  # otherwise it'll throw an error
   private def comment_params
     params.require(:comment).permit(:username, :body)
   end
