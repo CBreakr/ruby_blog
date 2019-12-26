dfa81a034cf38152a12e5eae889a69935787fed8
CBreakr <rob.notwicz@gmail.com>
2019-12-26 01:55:46 -0500

    add new post, initial style template

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,31 @@
 class PostsController < ApplicationController
   def index
-    
+    @posts = Post.all
+  end
+
+  def new
+
+  end
+
+  def show
+    # pass in the ID
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    # just to show that the submission
+    # routes to this method
+    #render plain: params[:post].inspect;
+
+    # actually create the new post in the DB
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+  private def post_params
+    # need to specify the fields
+    # otherwise it'll throw an error
+    params.require(:post).permit(:title, :body)
   end
 end
