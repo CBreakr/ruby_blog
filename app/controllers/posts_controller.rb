4bf8473bab38c58682bd2a0a6652f4cb1af3dee2
CBreakr <rob.notwicz@gmail.com>
2019-12-26 02:36:03 -0500

    update post functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,32 @@
 class PostsController < ApplicationController
+  ###
+  # list all posts
   def index
     @posts = Post.all
   end
 
+  ###
+  # show individual post
   def show
-    # pass in the ID
+    # pass in the ID to get the particular form
     @post = Post.find(params[:id])
   end
 
+  ###
+  # new post form
   def new
     @post = Post.new
   end
 
+  ###
+  # action on new post submit
   def create
-    # just to show that the submission
-    # routes to this method
+    # just to show that the submission routes to this method
     #render plain: params[:post].inspect;
 
     # actually create the new post in the DB
     @post = Post.new(post_params)
+
     if(@post.save)
       redirect_to @post
     else
@@ -26,9 +34,27 @@ class PostsController < ApplicationController
     end
   end
 
+  ###
+  # edit post form
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  ###
+  # action on post update
+  def update
+    @post = Post.find(params[:id])
+    if(@post.update(post_params))
+      redirect_to @post
+    else
+      render "edit"
+    end
+  end
+
+  ###
+  # need to specify the fields
+  # otherwise it'll throw an error
   private def post_params
-    # need to specify the fields
-    # otherwise it'll throw an error
     params.require(:post).permit(:title, :body)
   end
 end
